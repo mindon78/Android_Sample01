c46e8cf93d6307509c0ff9b19dc165d29b13b3d9
ht.kim <mindon78@naver.com>
2017-07-18 14:40:51 +0900

    안드로이드 4일차  - Coffee 주문앱 layout

---
+++ app/src/main/java/com/ht/sample01/CoffeeActivity.java
@@ -0,0 +1,16 @@
+package com.ht.sample01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CoffeeActivity extends AppCompatActivity {
+
+    // 메인
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 레이아웃 설정 부분
+        setContentView(R.layout.activity_coffee);
+    }
+}
